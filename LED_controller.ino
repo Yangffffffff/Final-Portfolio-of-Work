192bc347ebc3d1ad168e204fbba485ac6648a52f
Yangffffffff <91968054+Yangffffffff@users.noreply.github.com>
2022-11-20 15:43:28 +0000

    Add files via upload
    The product selected Ultrasonic Distance Sensor (HC-SR04), buzzer, BTF-LIGHTING WS2812B LED Pixel Strip as the main body of the physical device. When the user approaches the glass human head model, the lights on the model's eyes will change with the user's distance. The user can see different light presentations by maintaining different interactive distances from the device. At distances > 61 cm < 69 cm, the light exhibits a blue to violet gradient. Distances > 51 cm < 59 cm, yellow to green gradient. Distances > 51 cm < 59 cm, yellow to green gradient. Distance > 41 cm < 49 cm, dark blue gradient. Distance >31cm <39cm, orange to yellow gradient. Distance >21cm <29cm, light purple When the user's distance is less than 19cm, the led light strip will go out and the buzzer will sound.

---
+++ LED_controller.ino
@@ -0,0 +1,217 @@
+#include <FastLED.h>
+
+FASTLED_USING_NAMESPACE
+
+#define DATA_PIN    12
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+#define NUM_LEDS    30
+
+bool gReverseDirection = false;
+
+CRGB leds[NUM_LEDS];
+
+const int trigPin = 2;
+const int echoPin = 3;
+int pinBuzzer = 5; 
+float cm;    
+
+long duration;
+int distance;
+
+#define COOLING  80
+#define SPARKING 120
+
+#define BRIGHTNESS          255
+#define FRAMES_PER_SECOND  120
+
+void setup() {
+  // put your setup code here, to run once:
+  delay(3000); // 3 second delay for recovery
+  
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // set master brightness control
+  FastLED.setBrightness(BRIGHTNESS);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(5, OUTPUT); 
+  Serial.begin(9600);
+}
+
+typedef void (*SimplePatternList[])();
+SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
+
+uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(5, LOW);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  if (duration < 3850) {
+    distance = duration*0.0343/2;
+  }
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  // send the 'leds' array out to the actual LED strip
+  FastLED.show();  
+  // insert a delay to keep the framerate modest
+  FastLED.delay(1000/FRAMES_PER_SECOND); 
+
+  // do some periodic updates
+  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0])
+
+   if ((distance >= 61) && (distance <= 69)) {  
+  gPatterns[gCurrentPatternNumber]();
+      digitalWrite(5, LOW);
+  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+
+   }
+  
+   if ((distance >= 51) && (distance <= 59)) {
+  // FastLED's built-in rainbow generator
+      digitalWrite(5, LOW);
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+
+  FastLED.show();
+  }
+
+  if ((distance >= 41) && (distance <= 49)) {
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+      digitalWrite(5, LOW);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+  }
+
+  if ((distance >= 31) && (distance <= 39)) {
+  // Array of temperature readings at each simulation cell
+      digitalWrite(5, LOW);
+      static byte heat[NUM_LEDS];
+
+  // Step 1.  Cool down every cell a little
+    for( int i = 0; i < NUM_LEDS; i++) {
+      heat[i] = qsub8( heat[i],  random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
+    }
+  
+    // Step 2.  Heat from each cell drifts 'up' and diffuses a little
+    for( int k= NUM_LEDS - 1; k >= 2; k--) {
+      heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2] ) / 3;
+    }
+    
+    // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
+    if( random8() < SPARKING ) {
+      int y = random8(7);
+      heat[y] = qadd8( heat[y], random8(160,255) );
+        digitalWrite(5, LOW);
+    }
+
+    // Step 4.  Map from heat cells to LED colors
+    for( int j = 0; j < NUM_LEDS; j++) {
+      CRGB color = HeatColor( heat[j]);
+      int pixelnumber;
+      if( gReverseDirection ) {
+        pixelnumber = (NUM_LEDS-1) - j;
+      } else {
+        pixelnumber = j;
+      }
+      leds[pixelnumber] = color;
+    }
+  }
+
+  if ((distance >= 21) && (distance <= 29)) {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  digitalWrite(5, LOW);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+    FastLED.show();
+  }
+  
+  if ((distance >= 2) && (distance <= 19)) {
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  digitalWrite(5, HIGH);
+  }
+}
+
+#define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
+
+void nextPattern()
+{
+  // add one to the current pattern number, and wrap around at the end
+  gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( leds, NUM_LEDS, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    leds[ random16(NUM_LEDS) ] += CRGB::White;
+  }
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( leds, NUM_LEDS, 10);
+  int pos = random16(NUM_LEDS);
+  leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  int pos = beatsin16( 13, 0, NUM_LEDS-1 );
+  leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_LEDS; i++) { //9948
+    leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( leds, NUM_LEDS, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
+}
